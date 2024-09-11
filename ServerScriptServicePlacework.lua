8ec7c042f78722021234f0320c8d15ad4f399043
gap <mp120805a@gmail.com>
2024-09-11 08:43:02 +0200

    #1 - Aggiornati 3 file

--- ServerScriptServicePlacework.lua
+++ ServerScriptServicePlacework.lua
@@ -1,9 +1,9 @@
 game:GetService("ReplicatedStorage"):WaitForChild("ReplicatedStoragePlacework").OnServerInvoke = function()
 	local HttpService = game:GetService("HttpService")
 	
-	local placeworkRing = HttpService:GetAsync(""):split(",")
-	local placeworkWhitelist = HttpService:GetAsync(""):split(",")
-	local placeworkSpeed = HttpService:GetAsync(""):split(",")
+	local placeworkRing = HttpService:GetAsync("https://raw.githubusercontent.com/gapunitec/Placework/%231/Rings/PlaceworkRing1.txt"):split(",")
+	local placeworkWhitelist = HttpService:GetAsync("https://raw.githubusercontent.com/gapunitec/Placework/%231/PlaceworkWhitelist.txt"):split(",")
+	local placeworkSpeed = HttpService:GetAsync("https://raw.githubusercontent.com/gapunitec/Placework/%231/PlaceworkSpeed.txt"):split(",")
 	
 	for i, placeworkRingData in ipairs(placeworkRing) do
 		placeworkRing[i] = tonumber(placeworkRingData)
